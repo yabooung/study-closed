b9591922b63c567a442a1374d7cd99a10b53255f
park <horusr8@gmail.com>
2022-01-24 01:39:32 +0900

    first

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import react from 'react';
+
+import './App.css';
+import FunctionComponent from './R030_FunctionComponent'
+
+function App() {
+  return (
+   <div>
+     <h1>Start React 200! </h1>
+     <FunctionComponent contents="[This is FunctionComponent ]"/>
+   </div>
+  );
+}
+
+export default App;
---
+++ src/R030_FunctionComponent.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function R030_FunctionComponent(props) {
+    let {contents} = props
+
+  return <div>
+      <h2>{contents}</h2>
+  </div>;
+}
+
+export default R030_FunctionComponent;
